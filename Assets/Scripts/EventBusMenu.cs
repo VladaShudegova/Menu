d75994daba450dda5f0f897108193d73c8a3dfbf
Vlada Shudegova <vlada.shudegova@gmail.com>
2024-09-30 14:32:12 +0700

    Add loser and winner Pannel

---
+++ Assets/Scripts/EventBusMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class EventBusMenu
+{
+    public static Action<string> onLoadWinnerPanel;
+    public static Action<string> onLoadLoserPanel;
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject _winnerPanel, _loserPanel;
+
+    private static MenuManager _instance = null;
+    public static MenuManager Instance
+    {
+        get 
+        {
+            if(_instance == null)
+            {
+                var manager = new GameObject("Menu");
+                manager.AddComponent<MenuManager>();
+            }
+            return _instance;
+        }
+    }
+
+
+    private void OnEnable(){
+        EventBusMenu.onLoadWinnerPanel += ActivatePanel;
+        EventBusMenu.onLoadLoserPanel += ActivatePanel;
+        EventBusMenu.onLoadWinnerPanel += DeactivatePanel;
+        EventBusMenu.onLoadLoserPanel += DeactivatePanel;
+    }
+
+
+    private void OnDisable() {
+         EventBusMenu.onLoadWinnerPanel -= ActivatePanel;
+         EventBusMenu.onLoadLoserPanel += ActivatePanel;
+         EventBusMenu.onLoadWinnerPanel -= DeactivatePanel;
+        EventBusMenu.onLoadLoserPanel -= DeactivatePanel;
+    }
+
+
     public void Play()
     {
         SceneManager.LoadScene("Game");
@@ -18,5 +52,15 @@ public class MenuManager : MonoBehaviour
         #elif UNITY_STANDALONE
             Application.Quit(); //выход из игры
         #endif
+    }
+
+    public void ActivatePanel(string name)
+    {
+        _winnerPanel.SetActive(true);
+    }
+
+    public void DeactivatePanel(string name)
+    {
+        _winnerPanel.SetActive(false);
     }
 }
